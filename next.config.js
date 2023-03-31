2dd954994e561524ae0d8b901e29132d68f8f916
auxiliary-verb <8024666+auxiliary-verb@users.noreply.github.com>
2023-03-31 18:46:36 +0900

    nextjsの書き方に変更

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+export const repos = process.env.NEXT_PUBLIC_REPOSITORY_NAME || "";
+
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  basePath: `/${repos}`,
 }
 
 module.exports = nextConfig
--- src/controller/useContent.ts
+++ src/controller/useContent.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { content } from "../service/generateService";
 
 export function useContent(name: string){
-  const [path, setPath] = useState("image-not-found-icon.svg");
+  const [path, setPath] = useState("/image-not-found-icon.svg");
   useEffect(()=>{
     if(name !== ""){
       content.getByName(name).then((result)=>{
